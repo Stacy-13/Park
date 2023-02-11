f0c5a9287380284bf413538645d51c5713394fb6
Stacy-13 <nastyakis03@mial.ru>
2023-02-11 17:49:09 +0300

    park

--- MainWindow.Designer.cs
+++ MainWindow.Designer.cs
@@ -28,32 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.addButton = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.addOrderButton = new System.Windows.Forms.Button();
             this.exitButton = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.createReportButton = new System.Windows.Forms.Button();
+            this.enterHistoryButton = new System.Windows.Forms.Button();
+            this.postLabel = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.tabPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // addButton
+            // addOrderButton
             // 
-            this.addButton.Location = new System.Drawing.Point(605, 91);
-            this.addButton.Name = "addButton";
-            this.addButton.Size = new System.Drawing.Size(152, 48);
-            this.addButton.TabIndex = 10;
-            this.addButton.Text = "Сформировать заказ";
-            this.addButton.UseVisualStyleBackColor = true;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(47, 19);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(503, 348);
-            this.dataGridView1.TabIndex = 9;
+            this.addOrderButton.Location = new System.Drawing.Point(918, 170);
+            this.addOrderButton.Name = "addOrderButton";
+            this.addOrderButton.Size = new System.Drawing.Size(140, 23);
+            this.addOrderButton.TabIndex = 10;
+            this.addOrderButton.Text = "Сформировать заказ";
+            this.addOrderButton.UseVisualStyleBackColor = true;
+            this.addOrderButton.Visible = false;
             // 
             // exitButton
             // 
-            this.exitButton.Location = new System.Drawing.Point(636, 380);
+            this.exitButton.Location = new System.Drawing.Point(307, 397);
             this.exitButton.Name = "exitButton";
             this.exitButton.Size = new System.Drawing.Size(75, 23);
             this.exitButton.TabIndex = 13;
@@ -61,24 +65,128 @@
             this.exitButton.UseVisualStyleBackColor = true;
             this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(11, 11);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(902, 362);
+            this.tabControl1.TabIndex = 15;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2);
+            this.tabPage1.Size = new System.Drawing.Size(894, 336);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Товары";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(-4, 0);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(896, 334);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2);
+            this.tabPage2.Size = new System.Drawing.Size(894, 336);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Заказы";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-4, 0);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(898, 336);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // createReportButton
+            // 
+            this.createReportButton.Location = new System.Drawing.Point(918, 209);
+            this.createReportButton.Name = "createReportButton";
+            this.createReportButton.Size = new System.Drawing.Size(140, 23);
+            this.createReportButton.TabIndex = 17;
+            this.createReportButton.Text = "Сформировать отчет";
+            this.createReportButton.UseVisualStyleBackColor = true;
+            this.createReportButton.Visible = false;
+            // 
+            // enterHistoryButton
+            // 
+            this.enterHistoryButton.Location = new System.Drawing.Point(918, 249);
+            this.enterHistoryButton.Name = "enterHistoryButton";
+            this.enterHistoryButton.Size = new System.Drawing.Size(140, 23);
+            this.enterHistoryButton.TabIndex = 18;
+            this.enterHistoryButton.Text = "История входа";
+            this.enterHistoryButton.UseVisualStyleBackColor = true;
+            this.enterHistoryButton.Visible = false;
+            this.enterHistoryButton.Click += new System.EventHandler(this.enterHistoryButton_Click);
+            // 
+            // postLabel
+            // 
+            this.postLabel.AutoSize = true;
+            this.postLabel.Location = new System.Drawing.Point(709, 64);
+            this.postLabel.Name = "postLabel";
+            this.postLabel.Size = new System.Drawing.Size(0, 13);
+            this.postLabel.TabIndex = 19;
+            // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1091, 436);
+            this.Controls.Add(this.postLabel);
+            this.Controls.Add(this.enterHistoryButton);
+            this.Controls.Add(this.createReportButton);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.exitButton);
-            this.Controls.Add(this.addButton);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.addOrderButton);
             this.Name = "MainWindow";
             this.Text = "MainWindow";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.tabPage2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Button addButton;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button addOrderButton;
         private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button createReportButton;
+        private System.Windows.Forms.Button enterHistoryButton;
+        private System.Windows.Forms.Label postLabel;
     }
 }
--- MainWindow.cs
+++ MainWindow.cs
@@ -13,23 +13,74 @@ namespace Park
 {
     public partial class MainWindow : Form
     {
+        SqlConnection sqlC = new SqlConnection(@"Data Source=DESKTOP-RLMBPA9;Initial Catalog=Park;Integrated Security=True;");
+
+        private void Access_Level()
+        {
+            switch (Context.secutityLevel)
+            {
+                case 1:
+                    postLabel.Text = "Администратор";
+                    createReportButton.Visible = true;
+                    enterHistoryButton.Visible = true;
+                    break;
+                case 2:
+                    postLabel.Text = "Старший смены";
+                    addOrderButton.Visible = true;
+                    break;
+                case 3:
+                    postLabel.Text = "Продавец";
+                    addOrderButton.Visible = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
-            if (Context.secutityLevel == 0)
-            {
-                addButton.Visible = false;
-            }
+            Access_Level();
         }
 
         private void exitButton_Click(object sender, EventArgs e)
         {
-            MainWindow MW = new MainWindow();
-            MW.ShowDialog();
             this.Close();
         }
 
+        private void MainWindow_Load(object sender, EventArgs e)
+        {       
+            sqlC.Open();
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter();
+            DataTable servicesList = new DataTable();
+            DataTable ordersList = new DataTable();
+
+            string querystring = $"select * from Services";
+            SqlCommand command = new SqlCommand(querystring, sqlC);
 
 
+            sqlDataAdapter.SelectCommand = command;
+            sqlDataAdapter.Fill(servicesList);
+            dataGridView1.DataSource = servicesList;
+
+
+            querystring = $"select * from Orders";
+            command = new SqlCommand(querystring, sqlC);
+
+            sqlC.Close();
+            sqlDataAdapter.SelectCommand = command;
+            sqlDataAdapter.Fill(ordersList);
+
+            dataGridView2.DataSource = ordersList;
+            sqlC.Close();
+        }
+
+        private void enterHistoryButton_Click(object sender, EventArgs e)
+        {
+            AddWindow AW = new AddWindow();
+            this.Hide();
+            AW.ShowDialog();
+            this.Show();
+        }
     }
 }
